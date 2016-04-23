b6c4f10f7a288084ecbb9cfadb3701d6c46768ac
Kristian Jabilles <kristianjabilles@Kristians-MacBook-Pro.local>
2016-04-23 14:54:08 +0800

    removed all files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// Import modules
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Notice that we've organized all of our routes into a separate file.
-import AppRouter from './router';
-
-// Now we can attach the router to the 'root' element like this:
-ReactDOM.render( <AppRouter />, document.getElementById('container'));
